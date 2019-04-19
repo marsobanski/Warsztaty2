066b3de677b3e090455cccf3bc743559558ffef5
Marcin Sobański <m.c.sobanski@gmail.com>
2019-04-19 16:36:53 +0200

    Pierwszy Commit

---
+++ src/pl/coderslab/dao/UserDao.java
@@ -0,0 +1,37 @@
+package dao;
+
+import domain.User;
+
+public class UserDao {
+
+    private final String SELECT_ALL_QUERY = "select*from users";
+    private final String SELECT_BY_ID_QUERY = "select*from users";
+    private final String UPDATE_QUERY = "select*from users";
+    private final String CREATE_QUERY = "select*from users";
+    private final String DELETE_BY_ID_QUERY = "select*from users";
+
+
+    public User create (User user) {
+        return null;
+    }
+
+    public void update(User user) {
+
+    }
+
+    public void delete(User user) {
+
+    }
+
+    public void findId(User user) {
+
+    }
+
+    public User findById(User user) {
+
+    }
+
+    public void findAll(User user) {
+
+    }
+}
---
+++ src/pl/coderslab/utils/DbConector.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class DbConector {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/workshop_2?useSSl";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
+    public static
+}
